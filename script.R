0a882bcc2f33e1926153c1820ed4d4c8777dae42
19262489 <160538621+19262489@users.noreply.github.com>
2024-03-02 23:52:29 +0000

    Update script.R

--- script.R
+++ script.R
@@ -1,56 +1,55 @@
 source("require_packages.R")
-require_packages(c("httr", "xml2", "tidyverse", "tidytext", "syuzhet","lubridate")
-                 
-                 library(httr)
-                 library(xml2)
-                 library(tidyverse)
-                 library(tidytext)
-                 library(syuzhet)
-                 library(lubridate)
-                 
-                 # 1. HTTP GET Request to the CNN RSS feed URL
-                 RSS_URL <- "http://rss.cnn.com/rss/cnn_latest.rss."
-                 CNN <- GET(RSS_URL)
-                 
-                 # Parse the XML content
-                 xml_content <- read_xml(rawToChar(CNN$content))
-                 
-                 # 2. Perform sentiment analysis on the titles and descriptions of the news stories
-                 titles <- xml_content %>% xml_find_all("//item/title") %>% xml_text()
-                 descriptions <- xml_content %>% xml_find_all("//item/description") %>% xml_text()
-                 
-                 
-                 # Combine titles and descriptions
-                 text_data <- tibble(type = c(rep("title", length(titles)), rep("description", length(descriptions))),
-                                     text = c(titles, descriptions))
-                 
-                 
-                 # Tokenize the text
-                 tokenized_data <- text_data %>%
-                   unnest_tokens(word, text)
-                 
-                 # Perform sentiment analysis using the AFINN lexicon from the syuzhet package
-                 sentiment_analysis_data <- tokenized_data %>%
-                   mutate(score = get_sentiment(word, method = "afinn")) %>%
-                   group_by(type) %>%
-                   summarise(sentiment = sum(score, na.rm = TRUE))
-                 
-                 # 3. Summary statistics of the overall sentiment of the news stories
-                 summary_stats <- sentiment_analysis_data %>%
-                   summarise(mean_sentiment = mean(sentiment),
-                             sd_sentiment = sd(sentiment),
-                             min_sentiment = min(sentiment),
-                             max_sentiment = max(sentiment))
-                 
-                 
-                 # Print summary statistics
-                 print(summary_stats)
-                 
-                 
-                 # Print summary statistics and save to a timestamped file
-                current_datetime <- format(now(), "%Y-%m-%d_%H-%M-%S")
-                Data <- paste0("sentiment_summary_", current_datetime, ".txt")
-                sink(file = Data)
-                print(summary_stats)
-                sink()  # Close the connection to the file
-                 
+
+require_packages <- function(packages) {
+  for (pkg in packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      install.packages(pkg, dependencies = TRUE)
+      library(pkg, character.only = TRUE)
+    }
+  }
+}
+
+# Load required packages
+require_packages(c("httr", "xml2", "tidyverse", "tidytext", "syuzhet", "lubridate"))
+
+# Perform HTTP GET Request to the CNN RSS feed URL
+RSS_URL <- "http://rss.cnn.com/rss/cnn_latest.rss"
+CNN <- httr::GET(RSS_URL)
+
+# Parse the XML content
+xml_content <- xml2::read_xml(httr::content(CNN, as = "text"))
+
+# Extract titles and descriptions
+titles <- xml_content %>% xml2::xml_find_all("//item/title") %>% xml2::xml_text()
+descriptions <- xml_content %>% xml2::xml_find_all("//item/description") %>% xml2::xml_text()
+
+# Combine titles and descriptions
+text_data <- tibble::tibble(type = c(rep("title", length(titles)), rep("description", length(descriptions))),
+                            text = c(titles, descriptions))
+
+# Tokenize the text
+tokenized_data <- text_data %>%
+  tidytext::unnest_tokens(word, text)
+
+# Perform sentiment analysis using the AFINN lexicon from the syuzhet package
+sentiment_analysis_data <- tokenized_data %>%
+  dplyr::mutate(score = syuzhet::get_sentiment(word, method = "afinn")) %>%
+  dplyr::group_by(type) %>%
+  dplyr::summarise(sentiment = sum(score, na.rm = TRUE))
+
+# Calculate summary statistics of sentiment
+summary_stats <- sentiment_analysis_data %>%
+  dplyr::summarise(mean_sentiment = mean(sentiment),
+                    sd_sentiment = sd(sentiment),
+                    min_sentiment = min(sentiment),
+                    max_sentiment = max(sentiment))
+
+# Print summary statistics
+print(summary_stats)
+
+# Save summary statistics to a timestamped file
+current_datetime <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
+Data <- paste0("sentiment_summary_", current_datetime, ".txt")
+sink(file = Data)
+print(summary_stats)
+sink()  # Close the connection to the file
