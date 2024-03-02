ec17d72dab600763d7427ef2597dcb9f578f0b8a
19262489 <160538621+19262489@users.noreply.github.com>
2024-03-02 14:28:30 +0000

    Update script.R

--- script.R
+++ script.R
@@ -1,17 +1,50 @@
 source("require_packages.R")
-require_packages(c(
-  "xml2",
-  "httr"
+require_packages(c("httr", "xml2", "tidyverse", "tidytext", "syuzhet","lubridate"
 ))
 
 
-# HTTP GET Request
-a <- GET("https://github.com/skamper1/GitHub-Actions-R-Template")
+ # 1. HTTP GET Request to the CNN RSS feed URL
+                 RSS_URL <- "http://rss.cnn.com/rss/cnn_latest.rss."
+                 CNN <- GET(RSS_URL)
+                 
+                 # Parse the XML content
+                 xml_content <- read_xml(rawToChar(CNN$content))
+                 
+                 # 2. Perform sentiment analysis on the titles and descriptions of the news stories
+                 titles <- xml_content %>% xml_find_all("//item/title") %>% xml_text()
+                 descriptions <- xml_content %>% xml_find_all("//item/description") %>% xml_text()
+                 
+                 
+                 # Combine titles and descriptions
+                 text_data <- tibble(type = c(rep("title", length(titles)), rep("description", length(descriptions))),
+                                     text = c(titles, descriptions))
+                 
+                 
+                 # Tokenize the text
+                 tokenized_data <- text_data %>%
+                   unnest_tokens(word, text)
+                 
+                 # Perform sentiment analysis using the AFINN lexicon from the syuzhet package
+                 sentiment_analysis_data <- tokenized_data %>%
+                   mutate(score = get_sentiment(word, method = "afinn")) %>%
+                   group_by(type) %>%
+                   summarise(sentiment = sum(score, na.rm = TRUE))
+                 
+                 # 3. Summary statistics of the overall sentiment of the news stories
+                 summary_stats <- sentiment_analysis_data %>%
+                   summarise(mean_sentiment = mean(sentiment),
+                             sd_sentiment = sd(sentiment),
+                             min_sentiment = min(sentiment),
+                             max_sentiment = max(sentiment))
+                 
+                 
+                 # Print summary statistics
+                 print(summary_stats)
 
-html <- read_html(rawToChar(a$content))
-
-forks_counter_element <- xml_find_first(html, ".//span[@id='repo-network-counter']")
-
-forks_counter <- xml_integer(forks_counter_element)
-
-forks_counter |>  as.character() |> paste(Sys.time()) |> writeLines(format(Sys.Date(), "%m-%Y.txt"))
+# Print summary statistics and save to a timestamped file
+                 current_datetime <- format(now(), "%Y-%m-%d_%H-%M-%S")
+                 Data <- paste0("sentiment_summary_", current_datetime, ".txt")
+                 sink(file = Data)
+                 print(summary_stats)
+                 sink()  # Close the connection to the file
+#forks_counter |>  as.character() |> paste(Sys.time()) |> writeLines(format(Sys.Date(), "%m-%Y.txt"))
