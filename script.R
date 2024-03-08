c13ed89e9307ffe8d397e4edbcb5112b4a50ca3e
19262489 <160538621+19262489@users.noreply.github.com>
2024-03-08 14:57:39 +0000

    Update script.R

--- script.R
+++ script.R
@@ -2,41 +2,41 @@ source("require_packages.R")
 require_packages(c("httr", "xml2", "dplyr","stringr","readr", "tidyr", "syuzhet","lubridate")
 
 RSS_URL <- "http://rss.cnn.com/rss/cnn_latest.rss."
-                 CNN <- GET(RSS_URL)
+CNN <- GET(RSS_URL)
                  
                 
-                 xml_content <- read_xml(rawToChar(CNN$content))
+xml_content <- read_xml(rawToChar(CNN$content))
                  
-                 titles <- xml_content %>% xml_find_all("//item/title") %>% xml_text()
-                 descriptions <- xml_content %>% xml_find_all("//item/description") %>% xml_text()
-                 
-                 
-                 text_data <- tibble(type = c(rep("title", length(titles)), rep("description", length(descriptions))),
-                                     text = c(titles, descriptions))
+titles <- xml_content %>% xml_find_all("//item/title") %>% xml_text()
+descriptions <- xml_content %>% xml_find_all("//item/description") %>% xml_text()
                  
                  
-                 tokenized_data <- text_data %>%
-                   unnest_tokens(word, text)
-                 
-                 sentiment_analysis_data <- tokenized_data %>%
-                   mutate(score = get_sentiment(word, method = "afinn")) %>%
-                   group_by(type) %>%
-                   summarise(sentiment = sum(score, na.rm = TRUE))
-                 
-                 summary_stats <- sentiment_analysis_data %>%
-                   summarise(mean_sentiment = mean(sentiment),
-                             sd_sentiment = sd(sentiment),
-                             min_sentiment = min(sentiment),
-                             max_sentiment = max(sentiment))
+text_data <- tibble(type = c(rep("title", length(titles)), rep("description", length(descriptions))),
+text = c(titles, descriptions))
                  
                  
-                 print(summary_stats)
+tokenized_data <- text_data %>%
+unnest_tokens(word, text)
+                 
+sentiment_analysis_data <- tokenized_data %>%
+mutate(score = get_sentiment(word, method = "afinn")) %>%
+group_by(type) %>%
+summarise(sentiment = sum(score, na.rm = TRUE))
+                 
+summary_stats <- sentiment_analysis_data %>%
+summarise(mean_sentiment = mean(sentiment),
+sd_sentiment = sd(sentiment),
+min_sentiment = min(sentiment),
+max_sentiment = max(sentiment))
                  
                  
-                current_datetime <- format(now(), "%Y-%m-%d_%H-%M-%S")
-                Data <- paste0("sentiment_summary_", current_datetime, ".txt")
-                sink(file = Data)
-                print(summary_stats)
-                sink() 
+#print(summary_stats)
+                 
+                 
+current_datetime <- format(now(), "%Y-%m-%d_%H-%M-%S")
+Data <- paste0("sentiment_summary_", current_datetime, ".txt")
+sink(file = Data)
+print(summary_stats)
+sink() 
                  
                  
